90eddffa2cf1ca5df67d8b85c12999edeb490125
DragonScreech <dragon.screech76@gmail.com>
2026-02-13 11:42:28 -0600

    started the cast and crew page

---
+++ cast.js
@@ -0,0 +1,30 @@
+const PEOPLE = {
+  "Person 1": {
+    desc: "sdfnskjenrv;",
+    image: "assets/drama-club-900x600"
+  },
+  "Person 2": {
+    desc: "sdfnskjenrv;",
+    image: "assets/drama-club-900x600"
+  },
+  "Person 3": {
+    desc: "sdfnskjenrv;",
+    image: "assets/drama-club-900x600"
+  },
+  "Person 4": {
+    desc: "sdfnskjenrv;",
+    image: "assets/drama-club-900x600"
+  },
+  "Person 5": {
+    desc: "sdfnskjenrv;",
+    image: "assets/drama-club-900x600"
+  },
+  "Person 6": {
+    desc: "sdfnskjenrv;",
+    image: "assets/drama-club-900x600"
+  },
+  "Person 7": {
+    desc: "sdfnskjenrv;",
+    image: "assets/drama-club-900x600"
+  }
+}
--- nav.js
+++ nav.js
@@ -1,13 +1,16 @@
 const menuButton = document.querySelector("svg")
 const sidebar = document.querySelector(".sidebar")
 const closeButton = document.querySelector(".close-button")
+const body = document.querySelector("body")
 
 let toggle = false
 
 menuButton.addEventListener("click", () => {
   sidebar.style.display = "flex"
+  body.style.overflow = "hidden"
 })
 
 closeButton.addEventListener("click", () => {
   sidebar.style.display = "none";
+  body.style.overflow = "visible"
 })
